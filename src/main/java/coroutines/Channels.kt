7cffa43b769a059bfd61ad2fec18050202c22d73
snaplucas <lucas.chapola@gmail.com>
2017-08-20 23:40:24 -0300

    +Coroutines: Channels

---
+++ src/main/java/coroutines/Channels.kt
@@ -0,0 +1,19 @@
+@file:Suppress("EXPERIMENTAL_FEATURE_WARNING")
+
+package coroutines
+
+import kotlinx.coroutines.experimental.CommonPool
+import kotlinx.coroutines.experimental.channels.Channel
+import kotlinx.coroutines.experimental.launch
+import kotlinx.coroutines.experimental.runBlocking
+
+fun main(args: Array<String>) = runBlocking {
+    val channel = Channel<Int>()
+    launch(CommonPool) {
+        // this might be heavy CPU-consuming computation or async logic, we'll just send five squares
+        for (x in 1..5) channel.send(x * x)
+    }
+    // here we print five received integers:
+    repeat(5) { println(channel.receive()) }
+    println("Done!")
+}
