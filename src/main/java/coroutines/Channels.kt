d97a583c8d982086e77ca3d1c6f037b958c0fb7f
snaplucas <lucas.chapola@gmail.com>
2017-08-21 21:52:57 -0300

    update coroutines

--- src/main/java/coroutines/Channels.kt
+++ src/main/java/coroutines/Channels.kt
@@ -4,16 +4,75 @@ package coroutines
 
 import kotlinx.coroutines.experimental.CommonPool
 import kotlinx.coroutines.experimental.channels.Channel
+import kotlinx.coroutines.experimental.channels.consumeEach
+import kotlinx.coroutines.experimental.channels.produce
+import kotlinx.coroutines.experimental.delay
 import kotlinx.coroutines.experimental.launch
 import kotlinx.coroutines.experimental.runBlocking
+import kotlin.coroutines.experimental.CoroutineContext
+import kotlin.system.measureTimeMillis
 
-fun main(args: Array<String>) = runBlocking {
+fun mainA(args: Array<String>) = runBlocking {
     val channel = Channel<Int>()
     launch(CommonPool) {
         // this might be heavy CPU-consuming computation or async logic, we'll just send five squares
         for (x in 1..5) channel.send(x * x)
+        channel.close() // we're done sending
     }
     // here we print five received integers:
     repeat(5) { println(channel.receive()) }
     println("Done!")
+}
+
+fun produceSquares() = produce(CommonPool) {
+    for (x in 1..5) send(x * x)
+}
+
+fun mainB(args: Array<String>) = runBlocking {
+    val squares = produceSquares()
+    squares.consumeEach { println(it) }
+    println("Done!")
+}
+
+//data class Ball(var hits: Int)
+//
+//fun main(args: Array<String>) = runBlocking<Unit> {
+//    val table = Channel<Ball>() // a shared table
+//    launch(coroutineContext) { player("ping", table) }
+//    launch(coroutineContext) { player("pong", table) }
+//    table.send(Ball(0)) // serve the ball
+//    delay(1000) // delay 1 second
+//    table.receive() // game over, grab the ball
+//}
+//
+//suspend fun player(name: String, table: Channel<Ball>) {
+//    for (ball in table) { // receive the ball in a loop
+//        ball.hits++
+//        println("$name $ball")
+//        delay(300) // wait a bit
+//        table.send(ball) // send the ball back
+//    }
+//}
+
+suspend fun massiveRun(context: CoroutineContext, action: suspend () -> Unit) {
+    val n = 1000 // number of coroutines to launch
+    val k = 1000 // times an action is repeated by each coroutine
+    val time = measureTimeMillis {
+        val jobs = List(n) {
+            launch(context) {
+                repeat(k) { action() }
+            }
+        }
+        jobs.forEach { it.join() }
+    }
+    println("Completed ${n * k} actions in $time ms")
+}
+
+var counter = 0
+
+fun main(args: Array<String>) = runBlocking {
+    massiveRun(CommonPool) {
+        counter++
+    }
+    println("Counter = $counter")
 }
