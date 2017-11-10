736184803261a36021373bc9690b52ce5fb89fb9
snaplucas <lucas.chapola@gmail.com>
2017-11-10 18:22:28 -0200

    bug fix

--- src/test/kotlin/coroutines/CoroutinesTest.kt
+++ src/test/kotlin/coroutines/CoroutinesTest.kt
@@ -1,33 +1,26 @@
 package coroutines
 
+import kotlinx.coroutines.experimental.CommonPool
 import kotlinx.coroutines.experimental.Deferred
+import kotlinx.coroutines.experimental.async
 import kotlinx.coroutines.experimental.runBlocking
 import org.assertj.core.api.Assertions.assertThat
-import org.testng.annotations.BeforeMethod
 import org.testng.annotations.Test
-import shared.ContentDuration
-import shared.DummyService
 import java.time.Duration
 import java.time.Instant
 
 class CoroutineTest {
 
-    private lateinit var services: Array<String>
-    private lateinit var lambdas: List<DummyService>
-
-    @BeforeMethod internal fun setUp() {
-        services = arrayOf("Service A", "Service B", "Service C", "Service X", "Service Y", "Service Z")
-        lambdas = services.map { DummyService(it) }
-    }
-
     private val DummyService.asyncContent: Deferred<ContentDuration>
-        get() = kotlinx.coroutines.experimental.async(kotlinx.coroutines.experimental.CommonPool) { content }
+        get() = async(CommonPool) { content }
 
     @Test
     fun shouldBeParallel() {
+        val services = listOf("Service A", "Service B", "Service C", "Service X", "Service Y", "Service Z")
         val start = Instant.now()
         val results = runBlocking {
-            lambdas.map { it.asyncContent }
+            services.map { DummyService(it) }
+                    .map { it.asyncContent }
                     .map { it.await() }
         }
         val end = Instant.now()
--- src/test/kotlin/coroutines/DummyService.kt
+++ src/test/kotlin/coroutines/DummyService.kt
@@ -1,4 +1,4 @@
-package shared
+package coroutines
 
 import java.security.SecureRandom
 
